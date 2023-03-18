7a9715fe19a7b2454f70220a36738b1da5763070
Rafał Kowalski <rafkow91@yahoo.com>
2023-03-18 22:48:12 +0100

    change .gitignore, refactor code

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
 import os
-from data_processing import python_dict
-from datetime import datetime, date
+from data_processing import check_result_file
+from datetime import datetime
 
 
-
+offer_json_url = 'https://justjoin.it/api/offers'
 
 app = FastAPI()
 
-juniors_earnings = {
-    'date_last_update': datetime.fromtimestamp(os.path.getmtime('./result.csv')).date(),
-    'python': python_dict,
-}
 
 @app.get('/pystart-api')
 async def get_juniors_earnings():
-    return juniors_earnings
+    return {
+        'date_last_update': datetime.fromtimestamp(os.path.getmtime('./result.csv')),
+        'python': check_result_file(),
+    }
